62046758cf903882ec0f44edbd8dac350cc227b2
avramkjiprovski <avramkjiprovski@gmail.com>
2021-04-28 18:09:17 +0200

    Update botSync.js

--- botSync.js
+++ botSync.js
@@ -4,16 +4,9 @@ const config = require("./config.json")
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-//   client.guilds.cache.map((guild) => {
-//       console.log("map:", guild)
-//   })
 });
 
 client.on("guildBanAdd", function(guild, user){
-    // [x] - find a way to traverse guilds
-    // [x] - on ban in one guild, ban the same user in all other guilds.
-    // [] - check audit logs
-    // 
         client.guilds.cache.map((guild) => {
             guild.fetchBan(user).then((user)=>{
                 console.log(`${user.user.username} is already banned`)
@@ -40,13 +33,6 @@ client.on("guildBanRemove", function(guild, user){
             console.log(`\n${user.username} is not banned in ${guild.name}`)
         })
     })
-    
-    // client.guilds.cache.map((guild) => {
-    //     guild.members.unban(user.id)
-    //     .then(
-    //         user => console.log(`Unbanned ${user.username} from ${guild.name}`),
-    //         user => console.log(`${user} has already been unbanned from ${guild.name}`))
-    // })
 })
 
 client.login(config.token)

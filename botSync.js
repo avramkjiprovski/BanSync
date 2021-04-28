b36c9d3e6852b97f70e78640302f7d81472cf093
avramkjiprovski <avramkjiprovski@gmail.com>
2021-04-28 23:14:44 +0200

    Update botSync.js

--- botSync.js
+++ botSync.js
@@ -7,14 +7,15 @@ client.on('ready', () => {
 });
 
 client.on("guildBanAdd", function(guild, user){
-        
+        let reason = ""
         client.guilds.cache.map((guild) => {
             guild.fetchBan(user).then((user)=>{
                 // console.log(`${user.user.username} is already banned in ${guild.name} for ${user.reason}`)
+                reason = user.reason
             },
             ()=>{
-                guild.members.ban(user.id)
-                .then(user => console.log(`Banned ${user.username || user.id || user} from ${guild.name}`), error => console.log(error))
+                guild.members.ban(user.id, {reason: reason})
+                .then(user => console.log(`Banned ${user.username || user.id || user} from ${guild.name} for "${reason}"`), error => console.log(error))
             })
         })
 
